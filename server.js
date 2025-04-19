4b49a56e0e6a57824e05a616a4296db0ccdb5afb
fralcala <fralcala@iu.edu>
2025-04-18 21:20:06 -0400

    Update to server

--- server.js
+++ server.js
@@ -1,15 +1,35 @@
 const express = require("express");
 const path = require("path");
 
+// Create an express app
 const app = express();
 
-app.use(express.static(path.join(dirname, "views")));
+app.use(express.json());
 
-app.get("/*page/", function (req, res) {
-  res.sendFile(path.join(dirname, "views/404.html"));
+app.use("/api", require("./api"));
+
+app.use(
+  express.static(
+    path.join(__dirname, "views") //
+  )
+);
+
+app.get(
+  "/contact",
+  function (req, res, next) {
+    console.log(req.query);
+    next();
+  },
+  function (req, res, next) {
+    res.send("<p>Limit</p>");
+  }
+);
+
+app.get("/*page.html", function (req, res) {
+  res.redirect("/404.html");
 });
 
-// const port = process.env.PORT ||  ;
+const port = process.env.PORT || 13883;
 
 app.listen(port);
 
